d87f91d8c1120c56192b29132bc16c07f9cc853f
idzharism <idzharism@student.telkomuniversity.ac.id>
2022-04-23 14:03:40 +0700

    menambahkan API Movies

---
+++ jurnal08_kpl/Movies.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal08_kpl
+{
+    public class Movies
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+}
